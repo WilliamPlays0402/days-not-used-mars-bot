4c729627a9d72fbd39b201fbf5a651a46d3b2fb2
William Plays0402 <49759911+WilliamPlays0402@users.noreply.github.com>
2023-02-27 23:33:15 +0200

    Added core functionality

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+function updateTimer() {
+    let timeElement = document.getElementById("time");
+    let countDownDate = new Date("2022-08-06T09:26:00Z").getTime();
+    
+    setInterval(() => {
+        let now = new Date().getTime();
+        let timeDiff = countDownDate - now;
+    
+        let days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+        timeDiff %= (1000 * 60 * 60 * 24); // simplify with %=
+        
+        let hours = Math.floor(timeDiff / (1000 * 60 * 60));
+        timeDiff %= (1000 * 60 * 60); // simplify with %=
+        
+        let minutes = Math.floor(timeDiff / (1000 * 60));
+        timeDiff %= (1000 * 60); // simplify with %=
+        
+        let seconds = Math.floor(timeDiff / 1000);
+    
+        let timeString = days.toString().padStart(2, "0") + "d " 
+                        + hours.toString().padStart(2, "0") + "h " 
+                        + minutes.toString().padStart(2, "0") + "m " 
+                        + seconds.toString().padStart(2, "0") + "s";
+                        // use padStart() to format the time values
+    
+        timeElement.innerHTML = timeString;
+    }, 1000);
+}
 
+updateTimer();
